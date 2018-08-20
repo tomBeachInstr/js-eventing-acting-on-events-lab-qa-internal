f2b4fd204ad8dd04da17c5fbf05c1b5f81aaf186
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 14:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,44 +10,7 @@ dodger.style.bottom = '100px';
 console.log("left:", dodger.style.left);
 console.log("bottom:", dodger.style.bottom);
 
-document.addEventListener('keydown', moveDodger);
-
-function moveDodger(e) {
-  console.log("== moveDodger ==");
-  console.log("e.which:", e.which)
-  let whichKey = e.which;
-
-  switch(whichKey) {
-      case 37:
-          moveDodgerLeft()
-          break;
-      case 39:
-          moveDodgerRight()
-          break;
-      default:
-          // code block
-  };
-}
-
-function moveDodgerRight() {
-  console.log("== moveDodgerRight ==");
-  // removes "px" from left string so we can do math on actual number
-  var blackBox = document.getElementById("game");
-  var blackBoxL = blackBox.style.left;
-  console.log("blackBoxL:", blackBoxL);
-  var blackBoxW = blackBox.style.width.replace('px', '');
-  console.log("blackBoxW:", blackBoxW);
-  var rightLimit = parseInt(rightNumbers);
-  console.log("rightNumbers:", rightNumbers);
-
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers);
-
-  // prevents leaving black box
-  if (left < rightLimit) {
-    dodger.style.left = `${left + 10}px`
-  }
-}
+document.addEventListener('keydown', moveDodgerLeft);
 
 function moveDodgerLeft() {
   console.log("== moveDodgerLeft ==");
@@ -60,6 +23,43 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 10}px`
   }
 }
+
+// function moveDodger(e) {
+//   console.log("== moveDodger ==");
+//   console.log("e.which:", e.which)
+//   let whichKey = e.which;
+// 
+//   switch(whichKey) {
+//       case 37:
+//           moveDodgerLeft()
+//           break;
+//       case 39:
+//           moveDodgerRight()
+//           break;
+//       default:
+//           // code block
+//   };
+// }
+// 
+// function moveDodgerRight() {
+//   console.log("== moveDodgerRight ==");
+//   // removes "px" from left string so we can do math on actual number
+//   var blackBox = document.getElementById("game");
+//   var blackBoxL = blackBox.style.left;
+//   console.log("blackBoxL:", blackBoxL);
+//   var blackBoxW = blackBox.style.width.replace('px', '');
+//   console.log("blackBoxW:", blackBoxW);
+//   var rightLimit = parseInt(rightNumbers);
+//   console.log("rightNumbers:", rightNumbers);
+// 
+//   var leftNumbers = dodger.style.left.replace('px', '')
+//   var left = parseInt(leftNumbers);
+// 
+//   // prevents leaving black box
+//   if (left < rightLimit) {
+//     dodger.style.left = `${left + 10}px`
+//   }
+// }
 
 // NOTE: must click into browser window (not console)
 // document.addEventListener('keydown', function(e) {
