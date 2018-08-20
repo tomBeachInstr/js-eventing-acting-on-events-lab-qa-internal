0765febdece089caccd98361e46182dcf7b6dd84
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 14:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ dodger.style.bottom = '100px';
 console.log("left:", dodger.style.left);
 console.log("bottom:", dodger.style.bottom);
 
+document.addEventListener('keydown', moveDodgerLeft);
+
+function moveDodgerLeft() {
+  console.log("== moveDodgerLeft ==");
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
 // NOTE: must click into browser window (not console)
-document.addEventListener('keydown', function(e) {
-  console.log("-- keydown --");
-  console.log("e.which:", e.which)
-})
+// document.addEventListener('keydown', function(e) {
+//   console.log("-- keydown --");
+//   console.log("e.which:", e.which)
+// })
