f59101cd1aae5746859d78fb2d9d9cb98f99be29
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 14:46:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var dodger = document.getElementById('dodger');
 console.log("dodger:", dodger);
 dodger.style.backgroundColor = "red";
 
-console.log("left:", dodger.style.left);
-console.log("bottom:", dodger.style.bottom);
-
-dodger.style.bottom = '100px';
-
+// console.log("left:", dodger.style.left);
+// console.log("bottom:", dodger.style.bottom);
+// 
+// dodger.style.bottom = '100px';
+//
 console.log("left:", dodger.style.left);
 console.log("right:", dodger.style.right);
 console.log("top:", dodger.style.top);
