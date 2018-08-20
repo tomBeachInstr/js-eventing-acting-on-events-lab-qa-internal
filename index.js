2ded5f4aa33b73808d464e911bcdf0268aaab976
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 15:12:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers);
 
-  if (left < 380) {
+  if (left < 360) {
     dodger.style.left = `${left + 10}px`
   }
 }
