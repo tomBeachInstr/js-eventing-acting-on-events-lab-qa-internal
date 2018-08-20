2a4a3418178a59b42181ba026a468e0c31cd16ac
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 15:23:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,76 @@
+var dodger = document.getElementById('dodger');
+var game = document.getElementById('game');
+dodger.style.backgroundColor = "red";
+
+console.log("left:", dodger.style.left);
+console.log("right:", dodger.style.right);
+console.log("top:", dodger.style.top);
+console.log("bottom:", dodger.style.bottom);
+
+// dodger.style.bottom = '100px';
+// dodger.style.right = '200px';
+
+document.addEventListener('keydown', moveDodger);
+
+function moveDodger(e) {
+  console.log("== moveDodger ==");
+  console.log("e.which:", e.which)
+  if (e.which === 37) {
+    moveDodgerLeft();
+  } else if (e.which === 38) {
+    moveDodgerUp();
+  } else if (e.which === 39) {
+    moveDodgerRight();
+  } else if (e.which === 40) {
+    moveDodgerDown();
+  }
+}
+
+function moveDodgerUp() {
+  console.log("== moveDodgerUp ==");
+  // removes "px" from left string so we can do math on actual number
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers);
+
+  if (bottom < 380) {
+    dodger.style.bottom = `${bottom + 10}px`
+  }
+}
+
+function moveDodgerDown() {
+  console.log("== moveDodgerDown ==");
+  // removes "px" from left string so we can do math on actual number
+  var bottomNumbers = dodger.style.bottom.replace('px', '')
+  var bottom = parseInt(bottomNumbers);
+
+  if (bottom >= 0) {
+    dodger.style.bottom = `${bottom - 10}px`
+  }
+}
+
+function moveDodgerRight() {
+  console.log("== moveDodgerRight ==");
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`
+  }
+}
+
+function moveDodgerLeft() {
+  console.log("== moveDodgerLeft ==");
+  // removes "px" from left string so we can do math on actual number
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+}
+
+// NOTE: must click into browser window (not console)
+// document.addEventListener('keydown', function(e) {
+//   console.log("-- keydown --");
+//   console.log("e.which:", e.which)
+// })
