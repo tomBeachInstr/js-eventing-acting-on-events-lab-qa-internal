8e3bbcd0ee984d66dfc0b559ac4fa36f2ba678ab
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-20 15:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var dodger = document.getElementById('dodger');
 console.log("dodger:", dodger);
 dodger.style.backgroundColor = "red";
 
-// console.log("left:", dodger.style.left);
+console.log("right:", dodger.style.right);
 // console.log("bottom:", dodger.style.bottom);
 //
 dodger.style.bottom = '100px';
